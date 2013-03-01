4a2544b58560dccca4ce9d978e3df124c1963b51
RustyRadio <danielc@my.ccsu.edu>
2013-03-01 12:33:05 -0500

    Update Equipment_.java

--- model/Equipment_.java
+++ model/Equipment_.java
@@ -16,8 +16,3 @@ public class Equipment_
   public static volatile SingularAttribute<Equipment, Date> dueDate;
   public static volatile SingularAttribute<Equipment, Boolean> reserved;
 }
-
-/* Location:           /Users/Rusty-Mac/Downloads/ALOC_EDS/ALOC_v2.jar
- * Qualified Name:     model.Equipment_
- * JD-Core Version:    0.6.2
- */
