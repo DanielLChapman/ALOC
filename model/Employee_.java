5961351c0003a0920665fdeb81bb67206e2ab210
RustyRadio <danielc@my.ccsu.edu>
2013-03-01 12:32:15 -0500

    Update Employee_.java

--- model/Employee_.java
+++ model/Employee_.java
@@ -12,8 +12,3 @@ public class Employee_
   public static volatile SingularAttribute<Employee, String> firstName;
   public static volatile SingularAttribute<Employee, String> password;
 }
-
-/* Location:           /Users/Rusty-Mac/Downloads/ALOC_EDS/ALOC_v2.jar
- * Qualified Name:     model.Employee_
- * JD-Core Version:    0.6.2
- */
